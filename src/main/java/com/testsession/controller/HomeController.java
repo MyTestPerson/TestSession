60fd04be092484b4a84982d6cc8fb1e6da6b8275
romanych2021 <romanych_2021@list.ru>
2020-10-22 07:06:56 +1100

    Commit

--- src/main/java/com/testsession/controller/HomeController.java
+++ src/main/java/com/testsession/controller/HomeController.java
@@ -5,7 +5,6 @@ import com.testsession.service.ActiveUserService;
 import com.testsession.service.UserServiceJpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
