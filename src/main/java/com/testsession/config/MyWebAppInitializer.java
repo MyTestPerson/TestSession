b801c92aaef545fd71b73b8ddf6321efca93fe63
romanych2021 <romanych_2021@list.ru>
2020-10-24 08:00:42 +1100

    Commit

---
+++ src/main/java/com/testsession/config/MyWebAppInitializer.java
@@ -0,0 +1,14 @@
+package com.testsession.config;
+
+import org.springframework.security.web.session.HttpSessionEventPublisher;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+
+public class MyWebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext container) {
+        container.addListener(new HttpSessionEventPublisher());
+    }
+}
--- src/main/java/com/testsession/config/RootConfig.java
+++ src/main/java/com/testsession/config/RootConfig.java
@@ -3,11 +3,9 @@ package com.testsession.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
@@ -19,11 +17,6 @@ public class RootConfig {
     @Bean
     SessionRegistry sessionRegistry(){
         return new SessionRegistryImpl();
-    }
-
-    @Bean
-    public HttpSessionEventPublisher httpSessionEventPublisher() {
-        return new HttpSessionEventPublisher();
     }
 
 }
