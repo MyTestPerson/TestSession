aeabe9824985cfbc1a393fa395fd1f861924c367
romanych2021 <romanych_2021@list.ru>
2020-10-23 15:00:42 +1100

    Commit

--- src/main/java/com/testsession/security/SecurityConfig.java
+++ src/main/java/com/testsession/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.session.SessionRegistry;
 
 @Configuration
