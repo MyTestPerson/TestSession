c77d4b5e6ad596b6d5f535b9517fe64e19051119
romanych2021 <romanych_2021@list.ru>
2020-10-24 09:32:05 +1100

    Commit

--- src/main/java/com/testsession/service/ActiveUserServiceImpl.java
+++ src/main/java/com/testsession/service/ActiveUserServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class ActiveUserServiceImpl implements ActiveUserService, HttpSessionBindingListener {
+public class ActiveUserServiceImpl implements ActiveUserService {
 
     private final
     SessionRegistry sessionRegistry;
